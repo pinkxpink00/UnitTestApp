f76a8159f1d0bc7b0f55e2b214dbf2ec5e09062a
pinkxpink00 <kyperu07@gmail.com>
2023-09-28 23:40:23 +0300

    first test [xUnit]

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,27 +6,14 @@ namespace UnitTestApp.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+        
 
         public IActionResult Index()
         {
-            return View();
+            ViewData["Message"] = "Hello world!";
+            return View("Index");
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+     
     }
 }
